424601f50c47340de0ec5cd11bcab7f03756a612
Noah Schoonover <noah@skoonie.com>
2021-03-04 08:57:10 -0700

    Added Javadocs to Forest and Tree class files
    Added proper documentation to Forest and Tree. These files should be used as examples for further documentation and commenting.

--- Forest.java
+++ Forest.java
@@ -2,13 +2,35 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 import static java.lang.Math.*;
 
+//----------------------------------------------------------------------------------------------------------------------
+// ::Forest
+//
+/**
+ * The Forest class holds and manipulates an ArrayList of Tree objects.
+ *
+ * Includes methods for reading and writing to file, calculating the distance between two trees, and printing the
+ * coordinates of every tree in the forest.
+ *
+ * @author Garrett Evans
+ * @version 1.1
+ *
+ */
 public class Forest {
 
     List<Tree> trees = new ArrayList<>();
 
+    //--------------------------------------------------------------------------------------------------
+    // Forest::Forest
+    //
+    /**
+     * The default constructor currently instantiates with 20 randomly positioned trees.
+     *
+     * @todo the default constructor should create an empty forest, and another method should
+     *      be created to add random trees to it. Alternatively, we can overload the constructor
+     *      to take in the number of trees and other parameters for a random forest.
+     */
     public Forest() {
         for(int i = 0; i < 20; i++){
             Random rand = new Random();
@@ -20,52 +42,117 @@ public class Forest {
         }
     }
 
-    public void readFromFile() throws IOException {
-        File file = new File("./src/forest.txt");
+    //--------------------------------------------------------------------------------------------------
+    // Forest::readFromFile
+    //
+    /**
+     * Reads tree data from a CSV file and stores it in the forest.
+     *
+     * @todo This method should probably accept an enum value (APPEND or CLEAR)
+     *      to determine whether or not to clear before adding trees from the file.
+     *
+     * @param path the path to the CSV file to be read
+     * @throws IOException if the CSV file is not found
+     */
+    public void readFromFile(String path) throws IOException {
 
+        File file = new File(path);
         BufferedReader br = new BufferedReader(new FileReader(file));
 
+        this.trees.clear();
+
         String line;
+
         while ((line = br.readLine()) != null) {
-            System.out.println(line);
+            // System.out.println(line);
             String[] dets = line.split(",");
             int x = Integer.parseInt(dets[0]);
             int y = Integer.parseInt(dets[1]);
             int z = Integer.parseInt(dets[2]);
             this.trees.add(new Tree(x, y, z));
-            for (String string : dets) { System.out.println(string); }
-
+            // for (String string : dets) { System.out.println(string); }
         }
 
         br.close();
     }
 
-    public static double distance(Tree A, Tree B) {
-        double x1 = A.getX();
-        double y1 = A.getY();
-        double z1 = A.getZ();
+    //--------------------------------------------------------------------------------------------------
+    // Forest::distance
+    //
+    /**
+     * Calculates the euclidean distance between two trees.
+     *
+     * @param a the first tree
+     * @param b the second tree
+     * @return the distance between trees a and b.
+     */
+    public static double distance(Tree a, Tree b) {
+        double x1 = a.getX();
+        double y1 = a.getY();
+        double z1 = a.getZ();
 
-        double x2 = B.getX();
-        double y2 = B.getY();
-        double z2 = B.getZ();
+        double x2 = b.getX();
+        double y2 = b.getY();
+        double z2 = b.getZ();
 
         return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2) + pow(z2-z1, 2));
     }
 
-    public static void main(String[] args) throws IOException {
+    //--------------------------------------------------------------------------------------------------
+    // Forest::getTree
+    //
+    /**
+     * Returns the tree at a specified index in the forest.
+     *
+     * @todo should handle out of bounds indexing without crashing
+     *
+     * @param x the index value of the desired tree
+     * @return the Tree object, if found.
+     */
+    public Tree getTree(int x) {
+        return this.trees.get(x);
+    }
 
-        Forest forest = new Forest();
+    //--------------------------------------------------------------------------------------------------
+    // Forest::listTrees
+    //
+    /**
+     * Outputs the coordinates of every tree in the forest.
+     */
+    public void listTrees() {
         int i = 0;
-        for(Tree tree : forest.trees) {
+        for(Tree tree : this.trees) {
             i++;
             System.out.println("Tree " + i + "; " + tree);
         }
-
-        System.out.println("The distance between these two trees is: " + distance(forest.getTree(0), forest.getTree(2)));
+        System.out.println();
     }
 
-    public Tree getTree(int x) {
-        return this.trees.get(x);
+    //--------------------------------------------------------------------------------------------------
+    // Forest::main
+    //
+    /**
+     * Simple forest testing; instantiates and prints a random forest, then clears the forest
+     * and reads data from a file to print to console as well.
+     *
+     * @todo Should be tested with JUnit testing instead of using main function.
+     *
+     * @param args none
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        Forest forest = new Forest();
+        forest.listTrees();
+        System.out.println("The distance between the first two trees is: " +
+                distance(forest.getTree(0), forest.getTree(1)) + "\n");
+
+
+        forest.readFromFile("forest.txt");
+        forest.listTrees();
+        System.out.println("The distance between the first two trees is: " +
+                distance(forest.getTree(0), forest.getTree(1)) + "\n");
+
     }
 
 }
--- Tree.java
+++ Tree.java
@@ -1,12 +1,66 @@
+
+//----------------------------------------------------------------------------------------------------------------------
+// ::Tree
+//
+/**
+ * The Tree class holds euclidean coordinates for the location of a tree along with various helper functions.
+ */
 public class Tree {
+
     private double x;
     private double y;
     private double z;
     private int cut = -1;
 
-    public double getX() {
-        return x;
+
+    //--------------------------------------------------------------------------------------------------
+    // Tree::Tree
+    //
+    /**
+     * This constructor instantiates a new tree at the origin of the euclidean space.
+     */
+    public Tree() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     }
+
+    //--------------------------------------------------------------------------------------------------
+    // Tree::Tree
+    //
+    /**
+     * This constructor instantiates a new tree with specified coordinates.
+     *
+     * @todo can we use default parameter values here?
+     *
+     * @param x the x coordinate of the tree
+     * @param y the y coordinate of the tree
+     * @param z the z coordinate (elevation) of the tree
+     */
+    public Tree(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // Tree::toString
+    //
+    /**
+     * Overrides the inherited toString method to print coordinates.
+     *
+     * @return a formatted string of the x, y, and z coordinates.
+     */
+    @Override
+    public String toString() {
+        return String.format("x: " + getX() + ", y: " + getY() + ", z: " + getZ());
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // Tree Getters and Setters
+    //
+
+    public double getX() { return x; }
     public void setX(double x) { this.x = x; }
 
     public double getY() { return y; }
@@ -15,22 +69,6 @@ public class Tree {
     public double getZ() { return z; }
     public void setZ(double z) { this.z = z; }
 
-    public Tree() {
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-    }
-
-    public Tree(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
     public int getCut() {return cut;}
 
-    @Override
-    public String toString() {
-        return String.format("x: " + getX() + ", y: " + getY() + ", z: " + getZ());
-    }
 }

d0918e33fe2e37edaf57883a4802351863f3d98d
Myles-Willis <79949474+Myles-Willis@users.noreply.github.com>
2021-03-25 16:18:47 -0600

    Added FIleChooser to GUI
    Still need to copy file path into text field and add error message for typed inputs.

--- src/main/java/com/treemoval/gui/TreemovalController.java
+++ src/main/java/com/treemoval/gui/TreemovalController.java
@@ -2,10 +2,17 @@ package com.treemoval.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.SubScene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import com.treemoval.data.Forest;
+
+import java.io.File;
 
 public class TreemovalController {
 
@@ -23,6 +30,8 @@ public class TreemovalController {
     private Button exportButton;
     @FXML
     private SubScene forestSubScene;
+    @FXML
+    private Button fileFinderButton;
 
     public void filePathOnEnter(ActionEvent event) { // todo setFocus
         String fileLocation = filepathTextField.getText();
@@ -42,5 +51,36 @@ public class TreemovalController {
     public void exitButtonOnAction(ActionEvent event) {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void fileFinderButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR, "");
+        Alert confirmAlert = new Alert(Alert.AlertType.INFORMATION, "");
+
+        errorAlert.initModality(Modality.APPLICATION_MODAL);
+        errorAlert.initOwner(stage);
+        confirmAlert.initModality(Modality.APPLICATION_MODAL);
+        confirmAlert.initOwner(stage);
+
+        errorAlert.getDialogPane().setHeaderText(".csv file not selected. Try again.");
+
+        if(file != null) {
+            String selectedFile = file.getAbsolutePath();
+            String checkCsv = ".csv";
+
+            if(!selectedFile.substring(selectedFile.length() - 4).equals(checkCsv)) {
+                errorAlert.showAndWait();
+                System.out.println("Not a csv file!");
+            } else {
+                confirmAlert.getDialogPane().setHeaderText(selectedFile +" was selected");
+                confirmAlert.showAndWait();
+                System.out.println(selectedFile +" was selected");
+            }
+        }
     }
 }
